d853fb60ae9ec27074e5338fca775172afff7671
sourabh <sourabh@localhost.localdomain>
2022-02-23 14:26:38 +0530

    "MYG-88278326: Socket close is not thread safe and other cleanup"

--- src/java.base/share/classes/java/net/SocketImpl.java
+++ src/java.base/share/classes/java/net/SocketImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -211,7 +211,7 @@ public abstract class SocketImpl implements SocketOptions {
      * @since 1.3
      */
     protected void shutdownInput() throws IOException {
-      throw new IOException("Method not implemented!");
+        throw new IOException("Method not implemented!");
     }
 
     /**
@@ -231,7 +231,7 @@ public abstract class SocketImpl implements SocketOptions {
      * @since 1.3
      */
     protected void shutdownOutput() throws IOException {
-      throw new IOException("Method not implemented!");
+        throw new IOException("Method not implemented!");
     }
 
     /**
